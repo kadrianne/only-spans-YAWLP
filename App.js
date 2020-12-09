28c3a45727ee41bde8898001784100e1d0bbf02b
kadrianne <kristine.a.du@gmail.com>
2020-12-09 14:50:04 -0700

    Setup redux.

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const store = createStore(reducers)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store} >
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
